1808f72c1c594b8b188c200716540253e528f400
Pedro Lopez Mellado <pedrolome@outlook.es>
2022-06-09 15:57:28 +0200

    Added DB values and executable JAR

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -2,6 +2,11 @@
 */
 
   INSERT INTO articles (code, name, price, stock, description, available)
-    VALUES('123', 'Nike SB', 50.95, 80, 'Nike Skateboarding, primarily known as Nike SB, is the Nike brand for its line of shoes, clothing, and equipment for skateboarding.', '1');
+    VALUES('123SB', 'Nike SB', 50.95, 80, 'Nike Skateboarding, primarily known as Nike SB, is the Nike brand for its line of shoes, clothing, and equipment for skateboarding.', '1');
 
+  INSERT INTO articles (code, name, price, stock, description, available)
+    VALUES('967CO', 'Converse Chuck Taylor', 79.99, 120, 'These shoes Converse Chuck Taylor All Star Lift are going to become your allies for any look.', '1');
+
+  INSERT INTO articles (code, name, price, stock, description, available)
+    VALUES('534FS', 'Fila Santiago Energized', 34.99, 150, 'The Fila Santiago Energized shoes will accompany you in your outdoor runs on the mountain or trails.', '1');
 
